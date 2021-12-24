fe1cad8685dc2c31892cf3b594ea03cbe2f34d30
Nashua32 <56478217+Nashua32@users.noreply.github.com>
2021-12-24 14:21:55 +0100

    Update oltasok.js

--- fiuk_lovoldozos/oltasok.js
+++ fiuk_lovoldozos/oltasok.js
@@ -1,6 +1,6 @@
 //0. Tibi, 1. Csaba
 
-let insults = [["Kakifej vagy!", "Bénaaaa"], 
+let insults = [["Kakifej vagy!", "Bénaaaa", "Diákhitelképű!", "Még egy ilyen mendemonda, beleverlek a betonba!", "Rude vagy, mint egy kalaprúd!"], 
                
                
-               ["Nekem csorogsz, kis patak?", "El vagy pakolva!"]];
+               ["Nekem csorogsz, kis patak?", "El vagy pakolva!", "Buta vagy", "Gyere, gyere, gyere!", "*ráflexel*"]];
